54d5803a0bfa7bd8611beeab313202cd610f1b66
rdokamoto@gmail.com <ryan.okamoto@Ryans-MacBook-Pro.local>
2022-06-09 11:54:54 -0700

    reworked card.style properties and associated functions. Game now functions as solitaire without any facedown cards

--- js/main.js
+++ js/main.js
@@ -31,64 +31,64 @@ class Card {
 }
 
 // Objects for heart cards
-const HA = new Card('red', "heart", 1, 'HA');
-const H2 = new Card('red', "heart", 2, "H2");
-const H3 = new Card('red', "heart", 3, "H3");
-const H4 = new Card('red', "heart", 4, "H4");
-const H5 = new Card('red', "heart", 5, "H5");
-const H6 = new Card('red', "heart", 6, "H6");
-const H7 = new Card('red', "heart", 7, "H7");
-const H8 = new Card('red', "heart", 8, "H8");
-const H9 = new Card('red', "heart", 9, "H9");
-const H10 = new Card('red', "heart", 10, "H10");
-const HJ = new Card('red', "heart", 11, "HJ");
-const HQ = new Card('red', "heart", 12, "HQ");
-const HK = new Card('red', "heart", 13, "HK");
+const HA = new Card('red', "heart", 1, 'card hA');
+const H2 = new Card('red', "heart", 2, 'card h02');
+const H3 = new Card('red', "heart", 3, 'card h03');
+const H4 = new Card('red', "heart", 4, 'card h04');
+const H5 = new Card('red', "heart", 5, 'card h05');
+const H6 = new Card('red', "heart", 6, 'card h06');
+const H7 = new Card('red', "heart", 7, 'card h07');
+const H8 = new Card('red', "heart", 8, 'card h08');
+const H9 = new Card('red', "heart", 9, 'card h09');
+const H10 = new Card('red', "heart", 10, 'card h10');
+const HJ = new Card('red', "heart", 11, 'card hJ');
+const HQ = new Card('red', "heart", 12, 'card hQ');
+const HK = new Card('red', "heart", 13, 'card hK');
 
 //Objects for diamond cards
-const DA = new Card('red', "diamond", 1, "DA");
-const D2 = new Card('red', "diamond", 2, "D2");
-const D3 = new Card('red', "diamond", 3, "D3");
-const D4 = new Card('red', "diamond", 4, "D4");
-const D5 = new Card('red', "diamond", 5, "D5");
-const D6 = new Card('red', "diamond", 6, "D6");
-const D7 = new Card('red', "diamond", 7, "D7");
-const D8 = new Card('red', "diamond", 8, "D8");
-const D9 = new Card('red', "diamond", 9, "D9");
-const D10 = new Card('red', "diamond", 10, "D10");
-const DJ = new Card('red', "diamond", 11, "DJ");
-const DQ = new Card('red', "diamond", 12, "DQ");
-const DK = new Card('red', "diamond", 13, "DK");
+const DA = new Card('red', "diamond", 1, 'card dA');
+const D2 = new Card('red', "diamond", 2, 'card d02');
+const D3 = new Card('red', "diamond", 3, 'card d03');
+const D4 = new Card('red', "diamond", 4, 'card d04');
+const D5 = new Card('red', "diamond", 5, 'card d05');
+const D6 = new Card('red', "diamond", 6, 'card d06');
+const D7 = new Card('red', "diamond", 7, 'card d07');
+const D8 = new Card('red', "diamond", 8, 'card d08');
+const D9 = new Card('red', "diamond", 9, 'card d09');
+const D10 = new Card('red', "diamond", 10, 'card d10');
+const DJ = new Card('red', "diamond", 11, 'card dJ');
+const DQ = new Card('red', "diamond", 12, 'card dQ');
+const DK = new Card('red', "diamond", 13, 'card dK');
 
 // Objects for clover cards
-const CA = new Card('black', "clover", 1, "CA");
-const C2 = new Card('black', "clover", 2, "C2");
-const C3 = new Card('black', "clover", 3, "C3");
-const C4 = new Card('black', "clover", 4, "C4");
-const C5 = new Card('black', "clover", 5, "C5");
-const C6 = new Card('black', "clover", 6, "C6");
-const C7 = new Card('black', "clover", 7, "C7");
-const C8 = new Card('black', "clover", 8, "C8");
-const C9 = new Card('black', "clover", 9, "C9");
-const C10 = new Card('black', "clover", 10, "C10");
-const CJ = new Card('black', "clover", 11, "CJ");
-const CQ = new Card('black', "clover", 12, "CQ");
-const CK = new Card('black', "clover", 13, "CK");
+const CA = new Card('black', "clover", 1, 'card cA');
+const C2 = new Card('black', "clover", 2, 'card c02');
+const C3 = new Card('black', "clover", 3, 'card c03');
+const C4 = new Card('black', "clover", 4, 'card c04');
+const C5 = new Card('black', "clover", 5, 'card c05');
+const C6 = new Card('black', "clover", 6, 'card c06');
+const C7 = new Card('black', "clover", 7, 'card c07');
+const C8 = new Card('black', "clover", 8, 'card c08');
+const C9 = new Card('black', "clover", 9, 'card c09');
+const C10 = new Card('black', "clover", 10, 'card c10');
+const CJ = new Card('black', "clover", 11, 'card cJ');
+const CQ = new Card('black', "clover", 12, 'card cQ');
+const CK = new Card('black', "clover", 13, 'card cK');
 
 // Objects for spade cards
-const SA = new Card('black', "spade", 1, "SA");
-const S2 = new Card('black', "spade", 2, "S2");
-const S3 = new Card('black', "spade", 3, "S3");
-const S4 = new Card('black', "spade", 4, "S4");
-const S5 = new Card('black', "spade", 5, "S5");
-const S6 = new Card('black', "spade", 6, "S6");
-const S7 = new Card('black', "spade", 7, "S7");
-const S8 = new Card('black', "spade", 8, "S8");
-const S9 = new Card('black', "spade", 9, "S9");
-const S10 = new Card('black', "spade", 10, "S10");
-const SJ = new Card('black', "spade", 11, "SJ");
-const SQ = new Card('black', "spade", 12, "SQ");
-const SK = new Card('black', "spade", 13, "SK");
+const SA = new Card('black', "spade", 1, 'card sA');
+const S2 = new Card('black', "spade", 2, 'card s02');
+const S3 = new Card('black', "spade", 3, 'card s03');
+const S4 = new Card('black', "spade", 4, 'card s04');
+const S5 = new Card('black', "spade", 5, 'card s05');
+const S6 = new Card('black', "spade", 6, 'card s06');
+const S7 = new Card('black', "spade", 7, 'card s07');
+const S8 = new Card('black', "spade", 8, 'card s08');
+const S9 = new Card('black', "spade", 9, 'card s09');
+const S10 = new Card('black', "spade", 10, 'card s10');
+const SJ = new Card('black', "spade", 11, 'card sJ');
+const SQ = new Card('black', "spade", 12, 'card sQ');
+const SK = new Card('black', "spade", 13, 'card sK');
 
 
 const fullDeck = [HA, H2, H3, H4, H5, H6, H7, H8, H9, H10, HJ, HQ, HK, DA, D2, D3, D4, D5, D6, D7, D8, D9, D10, DJ, DQ, DK, CA, C2, C3, C4, C5, C6, C7, C8, C9, C10, CJ, CQ, CK, SA, S2, S3, S4, S5, S6, S7, S8, S9, S10, SJ, SQ, SK];
@@ -214,15 +214,12 @@ finishPile4.addEventListener('drop', cardDropFinishPile)
 function cardDrag(evt) {
   if(event.target.cards.length > 0) {
     clickSource = event.target.cards;
-  dragItem = event.target.cards[0];
-  dragSource = event.target;
+    dragItem = event.target.cards[0];
+    dragSource = event.target;
+    console.log(dragSource.cards[0].type)
   } else {}
 
 }
-
-// function testfun() {
-//   let dragSource = document.getElementById('dragSource.id')
-// } 
 
 function onDragOver(evt) {
   event.preventDefault();
@@ -231,34 +228,50 @@ function onDragOver(evt) {
 }
 
 function cardDropPlayPile(evt) {
-//  if (dragSource === placedCard) {
-//    console.log("placed card");
-//  }
+
+//DROP NEW KING
  if (dropTarget.cards.length === 0 && dragItem.value === 13) {
   dropTarget.cards.unshift(dragItem);
   clickSource.splice(0, 1);
-    if (dragSource.cards.length === 0) {
-      dragSource.innerHTML = '';
-      dropTarget.innerHTML = dragItem.type;
-    } else {dragSource.innerHTML = clickSource[0].type;
-  dropTarget.innerHTML = dragItem.type;}
+
+    // if (dragSource.cards.length === 0) {
+    //   dragSource.innerHTML = '';
+    //   dropTarget.innerHTML = dragItem.type;
+
+    // 
+  //   } else {dragSource.innerHTML = clickSource[0].type;
+  // dropTarget.innerHTML = dragItem.type;}
+
+  // DROP ON EMPY PILE NOT KING
  } else if (dropTarget.cards.length === 0) {
     clickSource.unshift(dragItem);
     clickSource.splice(0, 1);
+  // NORMAL DROP PLAY
   } else if (dragItem.red !== dropTarget.cards[0].red && dragItem.value === dropTarget.cards[0].value - 1) {
     dropTarget.cards.unshift(dragItem);
     clickSource.splice(0, 1);
-    if (dragSource.cards.length === 0) {
-      dragSource.innerHTML = '';
-    } else {dragSource.innerHTML = clickSource[0].type;}
 
-    creatCardObj();
 
+  // } else if(dragSource.cards.length === 0) {
+  //     dragSource.innerHTML = '';
+  //   } else {dragSource.innerHTML = clickSource[0].type;}
+
+  //   creatCardObj();
+
+// ILLEGAL PLAY
 } else {
   clickSource.unshift(dragItem);
   clickSource.splice(0, 1);
 }
 
+// PLACES CARD IMAGE
+if (dragSource.cards.length === 0) {
+  dropTarget.setAttribute('class', dropTarget.cards[0].type);
+  dragSource.setAttribute('class', 'none');
+} else {
+  dropTarget.setAttribute('class', dropTarget.cards[0].type);
+  dragSource.setAttribute('class', dragSource.cards[0].type);
+}
 // placedCardObj();
 // cardPlop()
 dropTarget = undefined;
@@ -302,27 +315,30 @@ dropTarget.appendChild(newCard);
 
 
 function cardDropFinishPile(evt) {
+  // ADDS NEW ACE
   if (dropTarget.cards.length < 1 && dragItem.value === 1) {
     dropTarget.cards.unshift(dragItem);
     clickSource.splice(0, 1);
-
-    if (dragSource.cards.length === 0) {
-      dragSource.innerHTML = '';
-      dropTarget.innerHTML = dragItem.type;
-    } else {dragSource.innerHTML = clickSource[0].type;
-    dropTarget.innerHTML = dragItem.type;}
-
+  // ATTEMPT TO ADD FIRST CARD THAT IS NOT AN ACE
   } else if (dropTarget.cards.length === 0) {
     clickSource.unshift(dragItem);
     clickSource.splice(0, 1); 
+  // LEGAL PLAY ON FINISH PILE
   } else if (dropTarget.cards[0].value === dragItem.value - 1 && dropTarget.cards[0].suit === dragItem.suit) {
     dropTarget.cards.unshift(dragItem);
     clickSource.splice(0, 1);
-    dragSource.innerHTML = clickSource[0].type;
-    dropTarget.innerHTML = dragItem.type;
+  // ILLEGAL PLAY ON FINISH PILE
   } else {
     clickSource.unshift(dragItem);
     clickSource.splice(0, 1); }
+      // PLACES CARD IMAGE
+    if (dragSource.cards.length === 0) {
+      dropTarget.setAttribute('class', dropTarget.cards[0].type);
+      dragSource.setAttribute('class', 'none');
+    } else {
+      dropTarget.setAttribute('class', dropTarget.cards[0].type);
+      dragSource.setAttribute('class', dragSource.cards[0].type);
+    }
   dropTarget = undefined;
   dragItem = undefined;
   clickSource = undefined;
@@ -343,26 +359,19 @@ function creatCardObj() {
 function gameStart() {
   shuffle();
   playPile1.cards.push(fullDeck[0]);
-  playPile1.cards[0].primed = true;
   fullDeck.splice(0, 1);
-  playPile2.cards.push(fullDeck[0], fullDeck[1]);
-  playPile2.cards[0].primed = true;
-  fullDeck.splice(0, 2);
-  playPile3.cards.push(fullDeck[0], fullDeck[1], fullDeck[2]);
-  playPile3.cards[0].primed = true;
-  fullDeck.splice(0, 3);
-  playPile4.cards.push(fullDeck[0], fullDeck[1], fullDeck[2], fullDeck[3]);
-  playPile4.cards[0].primed = true;
-  fullDeck.splice(0, 4);
-  playPile5.cards.push(fullDeck[0], fullDeck[1], fullDeck[2], fullDeck[3], fullDeck[4]);
-  playPile5.cards[0].primed = true;
-  fullDeck.splice(0, 5);
-  playPile6.cards.push(fullDeck[0], fullDeck[1], fullDeck[2], fullDeck[3], fullDeck[4], fullDeck[5]);
-  playPile6.cards[0].primed = true;
-  fullDeck.splice(0, 6);
-  playPile7.cards.push(fullDeck[0], fullDeck[1], fullDeck[2], fullDeck[3], fullDeck[4], fullDeck[5], fullDeck[6]);
-  playPile7.cards[0].primed = true;
-  fullDeck.splice(0, 7);
+  playPile2.cards.push(fullDeck[0]);
+  fullDeck.splice(0, 1);
+  playPile3.cards.push(fullDeck[0]);
+  fullDeck.splice(0, 1);
+  playPile4.cards.push(fullDeck[0]);
+  fullDeck.splice(0, 1);
+  playPile5.cards.push(fullDeck[0]);
+  fullDeck.splice(0, 1);
+  playPile6.cards.push(fullDeck[0]);
+  fullDeck.splice(0, 1);
+  playPile7.cards.push(fullDeck[0]);
+  fullDeck.splice(0, 1);
   while (fullDeck.length > 0) {
     playerDeck.cards.push(fullDeck[0]);
     fullDeck.splice(0, 1);
@@ -379,8 +388,10 @@ function deckClick(evt) {
   if (playerDeck.cards.length > 0) {
     playerPile.cards.unshift(playerDeck.cards[0]);
     playerDeck.cards.splice(0, 1);
-    playerPile.innerHTML = playerPile.cards[0].type;
+    // playerPile.innerHTML = playerPile.cards[0].type;
+    playerPile.setAttribute('class', playerPile.cards[0].type);
   } else {
+    playerPile.setAttribute('class', 'none')
     while (playerPile.cards.length > 0) {
       playerDeck.cards.unshift(playerPile.cards[0]);
       playerPile.cards.splice(0, 1);
@@ -412,44 +423,73 @@ function deckClick(evt) {
 function cardLocations() {
   // playerPile.innerHTML = playerPile.cards[0].type;
   if (playerPile.cards.length > 0) {
-    playerPile.innerHTML = playerPile.cards[0].type
+    playerPile.setAttribute('class', playerPile.cards[0].type);
   } else {
-    playerPile.innerHTML = "";}
+    playerPile.setAttribute('class', 'none');}
 
   if (playPile1.cards.length > 0) {
-    playPile1.innerText = playPile1.cards[0].type;
-  } else { playPile1.innerText = "" }
-  if (playPile2.cards.length > 0) {
-    playPile2.innerHTML = playPile2.cards[0].type;
-  } else { playPile2.innerHTML = "" }
-  if (playPile3.cards.length > 0) {
-    playPile3.innerHTML = playPile3.cards[0].type;
-  } else { playPile3.innerHTML = "" }
-  if (playPile4.cards.length > 0) {
-    playPile4.innerHTML = playPile4.cards[0].type;
-  } else { playPile4.innerHTML = "" }
-  if (playPile5.cards.length > 0) {
-    playPile5.innerHTML = playPile5.cards[0].type;
-  } else { playPile5.innerHTML = "" }
-  if (playPile6.cards.length > 0) {
-    playPile6.innerHTML = playPile6.cards[0].type;
-  } else { playPile6.innerHTML = "" }
-  if (playPile7.cards.length > 0) {
-    playPile7.innerHTML = playPile7.cards[0].type;
-  } else { playPile7.innerHTML = "" }
+    playPile1.setAttribute('class', playPile1.cards[0].type);
+  } else {playPile1.setAttribute('class', 'none');}
 
-  if (finishPile1.cards.length > 0) {
-    finishPile1.innerHTML = finishPile1.cards[0].type;
-  } else { finishPile1.innerHTML = "" }
-  if (finishPile2.cards.length > 0) {
-    finishPile2.innerHTML = finishPile2.cards[0].type;
-  } else { finishPile2.innerHTML = "" }
-  if (finishPile3.cards.length > 0) {
-    finishPile3.innerHTML = finishPile3.cards[0].type;
-  } else { finishPile3.innerHTML = "" }
-  if (finishPile4.cards.length > 0) {
-    finishPile4.innerHTML = finishPile4.cards[0].type;
-  } else { finishPile4.innerHTML = "" }
+  if (playPile2.cards.length > 0) {
+    playPile2.setAttribute('class', playPile2.cards[0].type);
+  } else {playPile2.setAttribute('class', 'none');}
+
+  if (playPile3.cards.length > 0) {
+    playPile3.setAttribute('class', playPile3.cards[0].type);
+  } else {playPile3.setAttribute('class', 'none');}
+  if (playPile4.cards.length > 0) {
+    playPile4.setAttribute('class', playPile4.cards[0].type);
+  } else {playPile4.setAttribute('class', 'none');}
+
+  if (playPile5.cards.length > 0) {
+    playPile5.setAttribute('class', playPile5.cards[0].type);
+  } else {playPile5.setAttribute('class', 'none');}
+
+  if (playPile6.cards.length > 0) {
+    playPile6.setAttribute('class', playPile6.cards[0].type);
+  } else {playPile6.setAttribute('class', 'none');}
+
+  if (playPile7.cards.length > 0) {
+    playPile7.setAttribute('class', playPile7.cards[0].type);
+  } else {playPile7.setAttribute('class', 'none');}
+
+
+
+
+
+
+  // if (playPile2.cards.length > 0) {
+  //   playPile2.innerHTML = playPile2.cards[0].type;
+  // } else { playPile2.innerHTML = "" }
+  // if (playPile3.cards.length > 0) {
+  //   playPile3.innerHTML = playPile3.cards[0].type;
+  // } else { playPile3.innerHTML = "" }
+  // if (playPile4.cards.length > 0) {
+  //   playPile4.innerHTML = playPile4.cards[0].type;
+  // } else { playPile4.innerHTML = "" }
+  // if (playPile5.cards.length > 0) {
+  //   playPile5.innerHTML = playPile5.cards[0].type;
+  // } else { playPile5.innerHTML = "" }
+  // if (playPile6.cards.length > 0) {
+  //   playPile6.innerHTML = playPile6.cards[0].type;
+  // } else { playPile6.innerHTML = "" }
+  // if (playPile7.cards.length > 0) {
+  //   playPile7.innerHTML = playPile7.cards[0].type;
+  // } else { playPile7.innerHTML = "" }
+
+  // if (finishPile1.cards.length > 0) {
+  //   finishPile1.innerHTML = finishPile1.cards[0].type;
+  // } else { finishPile1.innerHTML = "" }
+  // if (finishPile2.cards.length > 0) {
+  //   finishPile2.innerHTML = finishPile2.cards[0].type;
+  // } else { finishPile2.innerHTML = "" }
+  // if (finishPile3.cards.length > 0) {
+  //   finishPile3.innerHTML = finishPile3.cards[0].type;
+  // } else { finishPile3.innerHTML = "" }
+  // if (finishPile4.cards.length > 0) {
+  //   finishPile4.innerHTML = finishPile4.cards[0].type;
+  // } else { finishPile4.innerHTML = "" }
 }
 
 
